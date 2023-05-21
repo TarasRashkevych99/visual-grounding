91dd6189d97f496a76d57c8665b5efa38b6f0cc7
Taras Rashkevych <taras.rashkevych1999@gmail.com>
2023-05-21 03:11:40 +0200

    add: first version of actually functioning visual grounding

--- visual_grounding.py
+++ visual_grounding.py
@@ -6,6 +6,7 @@ from clip import clip
 import torch
 import os
 import numpy as np
+import cv2
 
 # Load a model
 # model = YOLO("yolov8n.yaml")  # build a new model from scratch
@@ -37,27 +38,30 @@ results = model("https://ultralytics.com/images/bus.jpg")  # predict on an image
 #         # print(f"probs: {result.probs}")
 
 
-def plot_bounding_boxes(results):
+def plot_bounding_boxes(img_path, results, indeces):
+    img = Image.open(img_path)
     fig, ax = plt.subplots()
-    ax.imshow(im)
+    ax.imshow(img)
     for result in results:
-        for xywh in result.boxes.xywh:
-            rect = patches.Rectangle(
-                (xywh[0] - xywh[2] / 2, xywh[1] - xywh[3] / 2),
-                xywh[2],
-                xywh[3],
-                linewidth=1,
-                edgecolor="r",
-                facecolor="none",
-            )
-            ax.add_patch(rect)
+        for index, xywh in enumerate(result.boxes.xywh):
+            print(result.boxes[index].cls)
+            if index in indeces:
+                rect = patches.Rectangle(
+                    (xywh[0] - xywh[2] / 2, xywh[1] - xywh[3] / 2),
+                    xywh[2],
+                    xywh[3],
+                    linewidth=1,
+                    edgecolor="r",
+                    facecolor="none",
+                    label=f"{result.boxes[index].cls}",
+                )
+                ax.add_patch(rect)
     plt.show()
 
 
 def clip_encoder(images, texts):
     images = torch.tensor(np.stack(images))
     text_tokens = clip.tokenize(texts)
-
     with torch.no_grad():
         images_z = clip_model.encode_image(images).float()
         texts_z = clip_model.encode_text(text_tokens).float()
@@ -77,18 +81,56 @@ def cosine_similarity(images_z: torch.Tensor, texts_z: torch.Tensor):
 
 
 if __name__ == "__main__":
+    model = YOLO("yolov8n.pt")
 
-    texts = ["Two people are standing in front of a bus.", "Road sign"]
+    clip_model, preprocess = clip.load("RN50")
+    clip_model = clip_model.eval()
+
+    texts = [
+        # "People walking on the street",
+        # "Two people walking",
+        # "Road sign",
+        "Person with black coat",
+    ]
     images = []
-    for filename in os.listdir("crops"):
-        if filename.endswith(".jpg") or filename.endswith(".png"):
-            file_path = os.path.join("crops", filename)
-            # print(file_path)
-            image = Image.open(file_path)
-            image = preprocess(image)
-            images.append(image)
+
+    results = model("bus.jpg")
+
+    im = Image.open("bus.jpg")
+    for result in results:
+        for index, xyxy in enumerate(result.boxes.xyxy):
+            cropped_img = im.crop(xyxy.numpy())
+            images.append(preprocess(cropped_img))
+
+    # for filename in os.listdir("crops"):
+    #     if filename.endswith(".jpg") or filename.endswith(".png"):
+    #         file_path = os.path.join("crops", filename)
+    #         print(file_path)
+    #         image = Image.open(file_path)
+    #         image = preprocess(image)
+    #         images.append(image)
 
     images_z, texts_z = clip_encoder(images, texts)
+
+    images_z /= images_z.norm(dim=-1, keepdim=True)
+    texts_z /= texts_z.norm(dim=-1, keepdim=True)
+
+    outputs = (100 * images_z @ texts_z.T).softmax(dim=0)
+
+    threshold = sum(outputs[:]) / len(outputs[:])
+    print(threshold)
+    print(outputs)
+
+    obj_indeces = [index for (index, prob) in enumerate(outputs[:]) if prob > threshold]
+
+    # res_plotted = results[0].plot()
+    # cv2.imshow("result", res_plotted)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+
+    plot_bounding_boxes("bus.jpg", results, obj_indeces)
+
+    _, predicted = outputs.max(1)
 
     for i in images_z:
         for j in texts_z:
