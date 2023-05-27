fe743d5fa4e71306766983c7c969ce5ac5b5ac78
Marco Rosson <marfiu99@gmail.com>
2023-05-27 13:20:44 +0200

    List from dictionary

--- src/load_dataset.py
+++ src/load_dataset.py
@@ -1,6 +1,7 @@
 import json 
 from PIL import Image
 import matplotlib.pyplot as plt
+import random
 
 instances_train = json.load(open("refcocog/annotations/instances_test.json"))
 
@@ -14,3 +15,16 @@ for key in instances_train.keys():
     rect = plt.Rectangle((bbox[0], bbox[1]), bbox[2], bbox[3], linewidth=1, edgecolor='g', facecolor='none')
     ax.add_patch(rect)
     plt.show()
+
+# instances_train_list = list(instances_train.values())
+# random.shuffle(instances_train_list)
+
+# for instance in instances_train_list:
+#     print(instance)
+#     _, ax = plt.subplots()
+#     im = Image.open("./refcocog/images/" + instance["image_name"])
+#     plt.imshow(im)
+#     bbox = instance["bbox"]
+#     rect = plt.Rectangle((bbox[0], bbox[1]), bbox[2], bbox[3], linewidth=1, edgecolor='g', facecolor='none')
+#     ax.add_patch(rect)
+#     plt.show()
