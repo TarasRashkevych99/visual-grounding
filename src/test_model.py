3ccf35b441fc83795fd68f6ba8554981b04f4b19
Taras Rashkevych <taras.rashkevych1999@gmail.com>
2023-07-09 18:07:29 +0200

    bla

--- src/test_model.py
+++ src/test_model.py
@@ -12,17 +12,17 @@ clip_model, preprocess = clip.load("RN50")
 test_set = CustomDataset(split="test", model=clip_model, transform=preprocess)
 
 model = DetachedHeadModel()
-model.load_state_dict(torch.load("real-model.pt", map_location=torch.device("cpu")))
+model.load_state_dict(torch.load("best-ever.pt", map_location=torch.device("cpu")))
 model.eval()
 
 with torch.no_grad():
-    for i in range(100, 150):
+    for i in range(30):
         image, text, bbox, category_id = test_set[i]
         predicted_bbox, unbound_class_probs = model(
             image.unsqueeze(0), text.unsqueeze(0)
         )
         predicted_class_id = torch.argmax(unbound_class_probs)
-        class_name = categories[predicted_class_id]
+        class_name = categories[predicted_class_id.item()]
         predicted_bbox = predicted_bbox[0]
         width, height = image.shape[1:]
         print("Intersection over union:", compute_iou(predicted_bbox, bbox))
@@ -33,10 +33,10 @@ with torch.no_grad():
         predicted_bbox = xywh_to_topleft(predicted_bbox.squeeze(0))
         ax.annotate(
             class_name,
-            (predicted_bbox[0], predicted_bbox[1]),
-            fontsize=8,
+            (predicted_bbox[0] * width, predicted_bbox[1] * height),
+            fontsize=12,
             fontweight="bold",
-            color="white",
+            color="red",
             ha="left",
             va="top",
         )
